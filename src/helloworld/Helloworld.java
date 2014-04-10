129f6f0a2690ea6304f3922c261643f1099052b5
aoi <aoi@172.19.6.232>
2014-04-10 11:22:32 +0900

    appendを直しました

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -15,6 +15,7 @@ public class Helloworld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.append("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("こんにちは世界");
     }
 }
